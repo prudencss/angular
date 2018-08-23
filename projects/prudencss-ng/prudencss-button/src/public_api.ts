cdcc481dbba092594f59666e622f3029059847f4
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2018-08-23 18:54:38 +0200

    cleanup

--- projects/prudencss-ng/prudencss-button/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of prudencss-button
- */
-
-export * from './lib/prudencss-button.service';
-export * from './lib/prudencss-button.component';
-export * from './lib/prudencss-button.module';
