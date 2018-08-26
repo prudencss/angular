25c0647289ea762242e3a8641dbe0fcd67805eec
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2018-08-26 23:01:58 +0200

    bump

--- projects/prudencss-ng/core/src/public_api.ts
+++ projects/prudencss-ng/core/src/public_api.ts
@@ -7,5 +7,7 @@ export * from "./lib/common-abstractions/component";
 
 export * from "./lib/common-behaviours/animation";
 export * from "./lib/common-behaviours/color";
+export * from "./lib/common-behaviours/decoration";
 export * from "./lib/common-behaviours/disabled";
+export * from "./lib/common-behaviours/size";
 export * from "./lib/common-behaviours/type";
