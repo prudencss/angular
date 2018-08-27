9f0770d494084729d045cdf47275c1dd99778007
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2018-08-27 21:09:41 +0200

    special treat because of native html behaviour on existing but undefined attr.disabled

--- projects/prudencss-ng/core/src/lib/common-behaviours/disabled.ts
+++ projects/prudencss-ng/core/src/lib/common-behaviours/disabled.ts
@@ -5,37 +5,36 @@ import { ComponentBaseProps } from "../common-abstractions/component";
 /** @docs-private */
 export interface CanDisable {
   /** Whether the component is disabled. */
-  disabled: boolean;
+  disabled?: DisabledPalette;
 }
+
+export type DisabledPalette = boolean | string | null | undefined;
 
 /** Mixin to augment a directive with a `disabled` property. */
 export abstract class Disabled extends ComponentBaseProps {
-  private _disabled: boolean = false;
-  private _disabledClass: string = "";
-
-  protected readonly _disabledClassCompanion: string = "u-pointer--disabled";
+  private _disabled: boolean;
+  private _disabledClass: string;
 
   get disabled() {
     return this._disabled;
   }
   set disabled(value: any) {
-    const _disabled = coerceBooleanProperty(value);
+    const _disabled = coerceBooleanProperty(value || null);
     const _disabledClass: string = typeof value === "string" ? value : "";
+    const _disabledClassCompanion: string = "u-pointer--disabled";
 
-    if (!_disabled !== this._disabled) {
-      if (this._disabledClass.length > 0) {
+    if (_disabled !== this._disabled) {
+      if (this._disabledClass && this._disabledClass.length > 0) {
         this._elementRef.nativeElement.classList.remove(this._disabledClass);
       }
       if (_disabled) {
-        this._elementRef.nativeElement.classList.add(
-          this._disabledClassCompanion
-        );
+        this._elementRef.nativeElement.classList.add(_disabledClassCompanion);
         if (_disabledClass.length > 0) {
           this._elementRef.nativeElement.classList.add(_disabledClass);
         }
       } else {
         this._elementRef.nativeElement.classList.remove(
-          this._disabledClassCompanion
+          _disabledClassCompanion
         );
       }
     }
