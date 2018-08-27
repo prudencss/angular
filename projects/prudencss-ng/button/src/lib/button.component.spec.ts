9ee15aba514eeb1ee652e6ee1ea9269d1aa73cc0
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2018-08-27 21:05:54 +0200

    refactoring

--- projects/prudencss-ng/button/src/lib/button.component.spec.ts
+++ projects/prudencss-ng/button/src/lib/button.component.spec.ts
@@ -1,25 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ButtonComponent } from './button.component';
+import { PrueButtonComponent } from "./button.component";
 
-describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+describe("PrueButtonComponent", () => {
+  let component: PrueButtonComponent;
+  let fixture: ComponentFixture<PrueButtonComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [PrueButtonComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(PrueButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
